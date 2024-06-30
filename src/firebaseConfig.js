1ddbf9309323bc1a45f94921ad1d50f434ae7d5e
Kevin Vulcano <kevin.vulcano@aggiemail.usu.edu>
2024-06-29 23:10:36 -0600

    config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,24 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: true && import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: true && import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: true && import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: true && import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: true && import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: true && import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY.substring(
+    0,
+    import.meta.env.VITE_FIREBASE_API_KEY.length - 2
+  ),
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
-console.log(firebaseConfig);
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
