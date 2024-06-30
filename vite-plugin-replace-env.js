0695cff5e585a077a8ffc4e1d1e1f41083f64e39
Kevin Vulcano <kevin.vulcano@aggiemail.usu.edu>
2024-06-30 11:18:17 -0600

    sadf

--- vite-plugin-replace-env.js
+++ vite-plugin-replace-env.js
@@ -2,15 +2,13 @@ export default function replaceEnvPlugin(envVars) {
   return {
     name: "replace-env",
     transform(code) {
-      let result = code;
+      let transformedCode = code;
       for (const [key, value] of Object.entries(envVars)) {
-        result = result.replace(
-          new RegExp(`import.meta.env.${key}`, "g"),
-          JSON.stringify(value)
-        );
+        const regex = new RegExp(`import\\.meta\\.env\\.${key}`, "g");
+        transformedCode = transformedCode.replace(regex, JSON.stringify(value));
       }
       return {
-        code: result,
+        code: transformedCode,
         map: null,
       };
     },
